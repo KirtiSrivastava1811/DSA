a6045f5f5a2551458ff2a0262b24884153f10cdc
kirti srivastava <146423753+KirtiSrivastava1811@users.noreply.github.com>
2025-03-30 10:37:51 +0530

    Add files via upload

---
+++ cookie.java
@@ -0,0 +1,16 @@
+public class cookie {
+
+    private String color;
+
+    public  cookie (String color){
+        this.color = color;
+    }
+
+public String getcolor(){
+    return color ;
+}
+ public void setcolor(String color){
+    this.color = color;
+ }
+}
+ 
