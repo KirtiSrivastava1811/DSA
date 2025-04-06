3e536f38b1c11fa1f8bbdc1b945b419181df8ebe
kirti srivastava <146423753+KirtiSrivastava1811@users.noreply.github.com>
2025-04-06 10:40:37 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array{
+    public static void main(String[] args) {
+        int[]marks=new int[3];
+        marks[0]=97;
+        marks[1]=87;
+        marks[2]=78;
+      //  System.out.println(marks);
+      for (int i = 0; i < 3; i++) {
+          System.out.println(marks[i]);
+      }
+    }
+ }
